285d65b4ef240a36ef041c3740b9cd3a404dd71a
Пригарин Вадим <tralalacav@mail.ru>
2023-01-12 13:21:07 +0300

    work

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Ingredient, Recipe, RecipeIngredient, Tag
 
 
-class RecipeIngredientInline(admin.StackedInline):
+class RecipeIngredientInline(admin.TabularInline):
     model = Recipe.ingredients
     min_num = 1
 
